8b9b11bd1089fa79e2072140f611a14e8b270b1e
Cooper2h <step.stepstep@yandex.ru>
2025-04-28 18:41:40 +0300

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,81 +1,79 @@
 function sleep(milliseconds) {
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
 async function getUserData(userId) {
-  try {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    if (!response.ok) {
-      throw new Error('Ошибка запроса');
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        if (!response.ok) {
+            throw new Error('Ошибка запроса');
+        }
+        return await response.json();
+    } catch (error) {
+        return null;
     }
-    return await response.json();
-  } catch (error) {
-    return null;
-  }
 }
 
 class AlarmClock {
-  constructor() {
-    this.alarmCollection = [];
-    this.intervalId = null;
-  }
-
-  addClock(time, callback, id) {
-    if (time === undefined || callback === undefined) {
-      throw new Error('Отсутствуют обязательные параметры');
-    }
-    if (id === undefined) {
-      throw new Error('Параметр id не передан');
-    }
-    if (this.alarmCollection.some(clock => clock.id === id)) {
-      console.error('Будильник с таким id уже существует');
-      return;
-    }
-    this.alarmCollection.push({ time, callback, id });
-  }
-
-  removeClock(id) {
-    this.alarmCollection = this.alarmCollection.filter(clock => clock.id !== id);
-  }
-
-  getCurrentFormattedTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    return `${hours}:${minutes}`;
-  }
-
-  start() {
-    if (this.intervalId !== null) {
-      return;
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
     }
 
-    const checkClock = (clock) => {
-      if (clock.time === this.getCurrentFormattedTime()) {
-        clock.callback();
-      }
-    };
-
-    this.intervalId = setInterval(() => {
-      this.alarmCollection.forEach(checkClock);
-    }, 1000);
-  }
-
-  stop() {
-    if (this.intervalId !== null) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
+    addClock(time, callback, id) {
+        if (id === undefined) {
+            throw new Error('Параметр id не передан');
+        }
+        if (this.alarmCollection.some(clock => clock.id === id)) {
+            console.error('Будильник с таким id уже существует');
+            return;
+        }
+        if (typeof callback !== 'function') {
+            throw new Error('Callback должен быть функцией');
+        }
+        if (!time) {
+            throw new Error('Время не передано');
+        }
+        this.alarmCollection.push({ time, callback, id });
     }
-  }
 
-  resetAllCalls() {
-    this.stop();
-    this.alarmCollection = [];
-  }
+    removeClock(id) {
+        this.alarmCollection = this.alarmCollection.filter(clock => clock.id !== id);
+    }
 
-  clearAlarmsWithoutTime() {
-    this.alarmCollection = this.alarmCollection.filter(clock => clock.time !== undefined);
-  }
+    getCurrentFormattedTime() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
+    }
+
+    start() {
+        if (this.intervalId !== null) {
+            return;
+        }
+
+        const checkClock = (clock) => {
+            if (clock.time === this.getCurrentFormattedTime()) {
+                clock.callback();
+            }
+        };
+
+        this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach(checkClock);
+        }, 1000);
+    }
+
+    stop() {
+        if (this.intervalId !== null) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    resetAllCalls() {
+        this.alarmCollection = [];
+    }
 }
 
 window.AlarmClock = AlarmClock;
