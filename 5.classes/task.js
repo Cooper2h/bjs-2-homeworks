74b699632f5a78ebe266e5f1706febaadabdb25b
Cooper2h <step.stepstep@yandex.ru>
2025-04-28 17:30:36 +0300

    Update task.js

---
+++ 5.classes/task.js
@@ -0,0 +1,28 @@
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    return this.books.find(book => book[type] === value) || null;
+  }
+
+  giveBookByName(bookName) {
+    const index = this.books.findIndex(book => book.name === bookName);
+    if (index !== -1) {
+      const book = this.books[index];
+      this.books.splice(index, 1);
+      return book;
+    }
+    return null;
+  }
+}
+
+window.Library = Library;
