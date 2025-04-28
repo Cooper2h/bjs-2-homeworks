f4f32a87fc1e5033442fe3d2236105834ff36a35
Cooper2h <step.stepstep@yandex.ru>
2025-04-28 17:05:59 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,14 @@
-function Student(name, gender, age) {
-  
+// Задача 1
+function getPercents(percent, number) {
+  return (percent / 100) * number;
 }
 
-Student.prototype.setSubject = function (subjectName) {
+// Задача 2
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let loanBody = amount - contribution;
+  let monthlyRate = (percent / 100) / 12;
+  let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
   
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+  return Number(totalAmount.toFixed(2));
 }
