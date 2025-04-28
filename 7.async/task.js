92e4a0ba3f70f725539a12e97b9421cee2c2fd8c
Cooper2h <step.stepstep@yandex.ru>
2025-04-28 18:10:35 +0300

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,76 +1,76 @@
 function sleep(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
 async function getUserData(userId) {
-    try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-        if (!response.ok) {
-            throw new Error('Ошибка запроса');
-        }
-        return await response.json();
-    } catch (error) {
-        return null;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    if (!response.ok) {
+      throw new Error('Ошибка запроса');
     }
+    return await response.json();
+  } catch (error) {
+    return null;
+  }
 }
 
 class AlarmClock {
-    constructor() {
-        this.alarmCollection = [];
-        this.intervalId = null;
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback, id) {
+    if (id === undefined) {
+      throw new Error('Параметр id не передан');
+    }
+    if (this.alarmCollection.some(clock => clock.id === id)) {
+      console.error('Звонок с таким id уже существует');
+      return;
+    }
+    this.alarmCollection.push({ id, time, callback });
+  }
+
+  removeClock(id) {
+    this.alarmCollection = this.alarmCollection.filter(clock => clock.id !== id);
+  }
+
+  getCurrentFormattedTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+
+  start() {
+    if (this.intervalId !== null) {
+      return;
     }
 
-    addClock(time, callback, id) {
-        if (id === undefined) {
-            throw new Error('Параметр id не передан');
-        }
+    const checkClock = (clock) => {
+      if (clock.time === this.getCurrentFormattedTime()) {
+        clock.callback();
+      }
+    };
 
-        if (this.alarmCollection.some(clock => clock.id === id)) {
-            console.error('Звонок с таким id уже существует');
-            return;
-        }
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach(clock => checkClock(clock));
+    }, 1000);
+  }
 
-        this.alarmCollection.push({ time, callback, id });
+  stop() {
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
     }
+  }
 
-    removeClock(id) {
-        this.alarmCollection = this.alarmCollection.filter(clock => clock.id !== id);
-    }
-
-    getCurrentFormattedTime() {
-        const now = new Date();
-        const hours = now.getHours().toString().padStart(2, '0');
-        const minutes = now.getMinutes().toString().padStart(2, '0');
-        return `${hours}:${minutes}`;
-    }
-
-    start() {
-        if (this.intervalId !== null) {
-            return;
-        }
-
-        const checkClock = (clock) => {
-            if (clock.time === this.getCurrentFormattedTime()) {
-                clock.callback();
-            }
-        };
-
-        this.intervalId = setInterval(() => {
-            this.alarmCollection.forEach(clock => checkClock(clock));
-        }, 1000);
-    }
-
-    stop() {
-        if (this.intervalId !== null) {
-            clearInterval(this.intervalId);
-            this.intervalId = null;
-        }
-    }
-
-    resetAllCalls() {
-        this.stop();
-        this.alarmCollection = [];
-    }
+  resetAllCalls() {
+    this.stop();
+    this.alarmCollection = [];
+  }
 }
 
 window.AlarmClock = AlarmClock;
+window.sleep = sleep;
+window.getUserData = getUserData;
