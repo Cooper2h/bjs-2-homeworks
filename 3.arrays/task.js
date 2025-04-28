2326e41284316fb9acaaae6c8cf3c5c9ec41c1ba
Cooper2h <step.stepstep@yandex.ru>
2025-04-28 16:37:25 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -4,3 +4,16 @@ function compareArrays(arr1, arr2) {
   }
   return arr1.every((element, index) => element === arr2[index]);
 }
+
+function getUsersNamesInAgeRange(users, gender) {
+  const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  const averageAge = totalAge / filteredUsers.length;
+
+  return averageAge;
+}
